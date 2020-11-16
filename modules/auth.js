d38f418b4962df20003a9dfbc57728dca2f75386
asus <sashmlv@yandex.ru>
2020-11-16 22:04:59 +0300

    wip.

--- modules/auth.js
+++ modules/auth.js
@@ -1,12 +1,31 @@
 'use strict';
 
 /**
- * Check access and redirect
+ * Check access and go forward
  * @param {object} req
  * @param {object} res
  * @return {undefined}
  **/
 function auth( req, res ) {
+
+   switch ( true ){
+
+      case '' === req.url:
+         return undefined;
+   }
+
+   return res.writeHead(
+      200, {
+
+         'Content-Type': 'application/json'
+      }
+   ).end( JSON.stringify({
+
+      name: 'AuthError',
+      message: 'Access denied',
+      code: 'ACCESS_DENIED',
+      status: 403,
+   }));
 
 };
 
