0bc0569c06d30dc8cefd192bcc6c9b266250c745
asus <sashmlv@yandex.ru>
2020-11-17 17:13:27 +0300

    Fix http.

--- modules/proxy.js
+++ modules/proxy.js
@@ -8,26 +8,13 @@ const {
    SSL_ENABLED,
 } = require( '../config' ),
    log = require( 'pino' )(),
-   http = require( 'http' ),
-   https = require( 'https' );
+   http = NODE_ENV === 'production' || SSL_ENABLED ? require( 'https' ) : require( 'http' );
 
 function proxy( req, res, { protocol, hostname, port }){
 
    return req.pipe(
 
-      protocol === 'https' ? https.request(
-         {
-            host: hostname,
-            port: port,
-            headers: req.headers,
-            method: req.method,
-            path: req.url,
-         },
-         response => {
-            res.writeHead( response.statusCode, response.headers );
-            response.pipe( res );
-         }
-      ) : http.request(
+      http.request(
          {
             host: hostname,
             port: port,
